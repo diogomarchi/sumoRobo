ae7ca7df213db22f211b346c4e32fff2a03e650f
diogomarchi <diogo.marchi@hotmail.com>
2021-03-27 21:09:41 -0300

    melhorado

--- AndroidStudio/app/src/main/java/com/example/sumorobo/MainActivity.java
+++ AndroidStudio/app/src/main/java/com/example/sumorobo/MainActivity.java
@@ -8,8 +8,13 @@ import android.view.MotionEvent;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Button;
-import android.view.View.OnLongClickListener;
 import android.view.View.OnTouchListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,128 +25,101 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //instanciando os botoes e caixa de texto
         bEsquerda = findViewById(R.id.bEsquerdaID);
         bDireita = findViewById(R.id.bDireitaID);
         bCima = findViewById(R.id.bCimaID);
         bBaixo = findViewById(R.id.bBaixoID);
         titulo = findViewById(R.id.textView);
 
+
+
+        // referencia ao banco e as variaveis
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbrRigthOn = database.getReference(
+                "rigth_on"
+        );
+        DatabaseReference dbrLeftOn = database.getReference(
+                "left_on"
+        );
+
+        // Enquanto presssiona botao esquerdo
         bEsquerda.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN ) {
                     titulo.setVisibility(View.INVISIBLE);
+                    dbrLeftOn.setValue(-1);
+                    dbrRigthOn.setValue(1);
                     return false;
                 }else{
                     titulo.setVisibility(View.VISIBLE);
+                    dbrLeftOn.setValue(0);
+                    dbrRigthOn.setValue(0);
                     return true;
                 }
             }
         });
 
 
-
+        // Enquanto presssiona botao direito
         bDireita.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN ) {
                     titulo.setVisibility(View.INVISIBLE);
+                    dbrLeftOn.setValue(1);
+                    dbrRigthOn.setValue(-1);
                     return false;
                 }else{
                     titulo.setVisibility(View.VISIBLE);
+                    dbrLeftOn.setValue(0);
+                    dbrRigthOn.setValue(0);
                     return true;
                 }
             }
         });
+
+        // Enquanto presssiona botao pra cima
         bCima.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN ) {
                     titulo.setVisibility(View.INVISIBLE);
+                    dbrLeftOn.setValue(1);
+                    dbrRigthOn.setValue(1);
                     return false;
                 }else{
                     titulo.setVisibility(View.VISIBLE);
+                    dbrLeftOn.setValue(0);
+                    dbrRigthOn.setValue(0);
                     return true;
                 }
             }
         });
+
+        // Enquanto presssiona botao pra baixo
         bBaixo.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN ) {
                     titulo.setVisibility(View.INVISIBLE);
+                    dbrLeftOn.setValue(-1);
+                    dbrRigthOn.setValue(-1);
                     return false;
                 }else{
                     titulo.setVisibility(View.VISIBLE);
+                    dbrLeftOn.setValue(0);
+                    dbrRigthOn.setValue(0);
                     return true;
                 }
             }
         });
-
     }
-
-
-/*
-    Button down = (Button) findViewById(R.id.bBaixo);
-    down.setOnLongClickListener(new OnLongClickListener() {
-        @Override
-        public boolean onLongClick(View v) {
-            Toast toast = Toast.makeText(getApplicationContext(), "TESTANDO", Toast.LENGTH_SHORT);
-            toast.show();
-            return true;
-        }
-    });
-    */
-
-
-/*
-    public void onClickDireita (View v){
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                View.OnLongClickListener
-                Toast toast = Toast.makeText(getApplicationContext(), "TESTANDO", Toast.LENGTH_SHORT);
-                toast.show();
-            case MotionEvent.ACTION_UP:
-                Toast toast2 = Toast.makeText(getApplicationContext(), "TESTANDO", Toast.LENGTH_SHORT);
-                toast2.show();
-        }
-    }
-
-
-    public void onClickEsquerda (MotionEvent event){
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                Toast toast = Toast.makeText(getApplicationContext(), "TESTANDO", Toast.LENGTH_SHORT);
-                toast.show();
-            case MotionEvent.ACTION_MOVE:
-            case MotionEvent.ACTION_UP:
-        }
-    }
-
-
-    public void onClickBaixo (MotionEvent event){
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                Toast toast = Toast.makeText(getApplicationContext(), "TESTANDO", Toast.LENGTH_SHORT);
-                toast.show();
-            case MotionEvent.ACTION_MOVE:
-            case MotionEvent.ACTION_UP:
-        }
-    }
-
-
-    public void onClickCima (MotionEvent event){
-        Toast toast = Toast.makeText(getApplicationContext(), "TESTANDO", Toast.LENGTH_SHORT);
-        toast.show();
-    }
-    */
-
-
-
-
-
 }
 
